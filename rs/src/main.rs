b2d6024593bb042061e46a9d7892f383b04dc29a
Nate Annau <nateannau@gmail.com>
2023-09-18 21:09:32 -0700

    Initial rust commit

---
+++ rs/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    println!("{}", raycast(dir, true));
+    // println!("Hello, world!");
+}
+
+struct Player {
+    x: f64,
+    y: f64,
+    dir: f64
+}
+
+fn raycast(angle: f64, x: bool) {
+    player = Player {x: 80.0, y: 80.0, dir: 0.01};
+    if x {
+        player_discrete: float64, player_cont: float64 = player.x, player.y;
+    }
+    player_discrete ;
+}
